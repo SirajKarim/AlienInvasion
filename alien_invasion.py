6f0fc4a80a9b1ef9a0ed440706c00cd76cea032e
SirajKarim <msirajakarim437@gmail.com>
2020-07-09 12:44:33 +0500

    Ship,Movement,Fire,Aliens all are done

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+import sys
+import pygame
+from settings import Setting
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+from alien import Alien    #ispe shak ha
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Setting()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    ship = Ship(ai_settings,screen)
+    alien = Alien(ai_settings,screen)   #ispe shak ha
+    bullets = Group()
+    aliens = Group()
+    pygame.display.set_caption("Alien Invasion")
+    gf.create_fleet(ai_settings,screen,aliens) #new
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        # bullets.update()
+        # Get rid of bullets that have disappeared.
+        # for bullet in bullets.copy():
+        #     if bullet.rect.bottom <= 0:
+        #         bullets.remove(bullet)
+        # print(len(bullets))
+        gf.update_bullets(bullets)                     
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)  #ispe shak tha
+        # update_bullets(bullets)
+
+run_game()
--- ship.py
+++ ship.py
@@ -1,35 +1,30 @@
 import pygame
-from pygame.sprite import Sprite
-
 
 class Ship():
-
-    def __init__(self,screen,ai_settings):
+    def __init__(self,ai_settings,screen):    #new
         self.screen = screen
-        self.image = pygame.image.load('images/ship.bmp')
+        self.ai_settings = ai_settings   #new
+        self.image = pygame.image.load('images/ship.bmp')  
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        
-        # self.ai_settings = ai_settings  
 
-        self.moving_right = False
-        self.moving_left = False
-
-        # self.center = float(self.rect.centerx)
-
+        self.center = float(self.rect.centerx) #new
+        #movement flag
+        self.move_right = False
+        self.move_left = False
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
 
     def update(self):
-        if self.moving_right: 
-             self.rect.centerx += 1
-            #  self.rect.centerx += self.ai_settings.ship_speed_factor
-        if self.moving_left:
-             self.rect.centerx -= 1
-            # self.rect.centerx -= self.ai_settings.ship_speed_factor
+        if self.move_right and self.rect.right < self.screen_rect.right:
+            # self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor  #new
+        elif self.move_left and self.rect.left > 0:
+            # self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor #new
 
-        # self.rect.centerx = self.center
+        self.rect.centerx = self.center
